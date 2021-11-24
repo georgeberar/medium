2b155d14e7980d586aa0b9685e4073801cdcc0ba
George Berar <georgeberar.contact@gmail.com>
2021-11-24 22:03:12 +0200

    initial commit for SpringBoot: Standardized API Error Handling post

---
+++ error-handling/src/test/java/com/example/errorhandling/ErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.errorhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
